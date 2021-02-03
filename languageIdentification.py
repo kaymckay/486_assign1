4098301dc7b3197403fcaaed1e01f11ef4e20ce5
Kayla <kaymckay@umich.edu>
2021-02-03 11:19:43 -0500

    done

---
+++ languageIdentification.py
@@ -0,0 +1,182 @@
+"""Kayla McKay (kaymckay)"""
+import os
+import sys
+import math 
+
+def trainBigramLanguageModel(text):
+    """Function to train a bigram language model. """
+    unigram = {}
+    diagram = {}
+    total = 0
+    prev = " "
+    for l in text:
+        if l.isupper():
+            l = l.lower()
+        #if l.isalpha() or l.isdigit() or l == "." or l == "'" or "(" or ")":
+        # if not l == "\n":
+        total += 1
+
+        #unigram
+        if l in unigram:
+            unigram[l] += 1
+        else:
+            unigram[l] = 1
+        
+        #diagram
+        if (prev + l) in diagram:
+            diagram[prev + l] += 1
+        else:
+            diagram[prev + l] = 1
+        prev = l
+
+    unigram["total"] = total
+    # return dictionary of character frequencies
+    return unigram, diagram
+
+
+
+def identifyLanguage(text, langs, unigrams, diagrams):
+    """Function to determine the language of a string.  """
+    # calculate english
+    p_e = 1
+    prev = ""
+    V = len(unigrams[0]) - 1
+    for t in text:
+        p_t = 0
+        # start
+        t = t.lower()
+        if prev == "":
+            val = unigrams[0][t] if (t in unigrams[0]) else 0 
+            p_t = math.log((val + 1) / (unigrams[0]["total"] +  V))
+        else:
+            val1 = diagrams[0][prev + t] if ((prev + t) in diagrams[0]) else 0
+            val2 = unigrams[0][prev] if (prev in unigrams[0]) else 0
+            p_t = math.log((val1 + 1) / (val2 + V))
+        p_e += p_t
+
+    # french
+    p_f = 1
+    prev = ""
+    V = len(unigrams[1]) - 1
+    for t in text:
+        p_t = 0
+        # start
+        t = t.lower()
+        if prev == "":
+            val = unigrams[1][t] if (t in unigrams[1]) else 0 
+            p_t = math.log((val + 1) / ( unigrams[1]["total"] +  V))
+        else:
+            val1 = diagrams[1][prev + t] if ((prev + t) in diagrams[1]) else 0
+            val2 = unigrams[1][prev] if (prev in unigrams[1]) else 0
+            p_t = math.log((val1 + 1) / (val2 + V))
+        p_f += p_t
+    
+    # italian
+    p_i = 1
+    prev = ""
+    V = len(unigrams[2]) - 1
+    for t in text:
+        p_t = 0
+        # start
+        t = t.lower()
+        if prev == "":
+            val = unigrams[2][t] if (t in unigrams[2]) else 0 
+            p_t = math.log((val + 1) / ( unigrams[2]["total"]  +  V))
+        else:
+            val1 = diagrams[2][prev + t] if ((prev + t) in diagrams[2]) else 0
+            val2 = unigrams[2][prev] if (prev in unigrams[2]) else 0
+            p_t = math.log((val1 + 1) / (val2 + V))
+        p_i += p_t
+    
+    winner = max([p_e, p_f, p_i]) 
+    if winner == p_e:
+        return langs[0]
+    if winner == p_f:
+        return langs[1]
+    return langs[2]
+
+
+
+
+
+def main(testfile):
+    directory = os.path.join('languageIdentification.data', 'training')
+    e = open(os.path.join(directory, "English"), 'r', encoding="ISO-8859-1")
+    f = open(os.path.join(directory, "French"), 'r', encoding="ISO-8859-1")
+    i = open(os.path.join(directory, "Italian"), 'r', encoding="ISO-8859-1")
+
+    # English
+    e_uni = {}
+    e_di = {}    
+    Lines = e.readlines()
+    for line in Lines:
+        uni, di = trainBigramLanguageModel(line)
+        for u in uni:
+            if u in e_uni:
+                e_uni[u] += uni[u]
+            else:
+                e_uni[u] = uni[u]
+        for d in di:
+            if d in e_di:
+                e_di[d] += di[d]
+            else:
+                e_di[d] = di[d]
+
+    # French
+    f_uni = {}
+    f_di = {}
+    Lines = f.readlines()
+    for line in Lines:
+        uni, di = trainBigramLanguageModel(line)
+        for u in uni:
+            if u in f_uni:
+                f_uni[u] += uni[u]
+            else:
+                f_uni[u] = uni[u]
+        for d in di:
+            if d in f_di:
+                f_di[d] += di[d]
+            else:
+                f_di[d] = di[d]
+
+    # Italian
+    i_uni = {}
+    i_di = {}
+    Lines = i.readlines()   
+    for line in Lines:
+        uni, di = trainBigramLanguageModel(line)
+        for u in uni:
+            if u in i_uni:
+                i_uni[u] += uni[u]
+            else:
+                i_uni[u] = uni[u]
+        for d in di:
+            if d in i_di:
+                i_di[d] += di[d]
+            else:
+                i_di[d] = di[d]
+
+
+    # Test
+    langs = ["English", "French", "Italian"]
+    t = open(testfile, 'r', encoding="ISO-8859-1")
+    Lines = t.readlines()
+    count = 1
+    out_file = open("languageIdentificaton.output", "w")
+    for line in Lines:
+        language = identifyLanguage(line, langs, [e_uni, f_uni, i_uni], [e_di, f_di, i_di])
+        print(line + " " + language, file=out_file)
+        count += 1
+
+
+
+    
+    
+
+
+
+
+
+if __name__ == '__main__':
+
+    main(sys.argv[1])
---
+++ preprocess.py
@@ -0,0 +1,429 @@
+"""Kayla McKay (kaymckay)"""
+
+import sys
+import os
+import re
+from stemmer import PorterStemmer
+
+def removeSGML(line):
+    """Function that removes SGML tags. """
+    # data = re.sub(r'<.*?>', '', line)
+    tag = False
+    quote = False
+    cleanedLine = ""
+
+    for char in line:
+        if char == '<' and not quote:
+            tag = True
+        elif char == '>' and not quote:
+            tag = False
+        elif (char == '"' or char == "'") and tag:
+            quote = not quote
+        elif not tag:
+            cleanedLine = cleanedLine + char 
+
+    # print(cleanedLine)
+    return cleanedLine
+
+
+def tokenizeText(line):
+    """Function that tokenizes the text. """
+    # initial split
+    line = line.strip('\n')
+    line = decontract(line)
+    # print(line)
+    line = line.split(' ')
+    tokens = []
+
+    # check for cases
+    token = ""
+    first = ""
+    second = ""
+    for word in line:
+        if not word == '':
+            # print(word)
+            for w in word:
+                # acronyms/abbreviatons
+                if  w == '.':
+                    if first == 'period':
+                        token = token[: len(token) - 1]
+                        tokens.append(token)
+                        token = '..'
+                        first, second = 'period', first
+                    elif first == 'cap' or first == "" or first == 'letter':
+                        token += w
+                        first, second = 'period', first
+                    elif first == "num":
+                        if w == word[len(word) - 1]:
+                            tokens.append(token)
+                            token = "."
+                        else:   
+                            token += w
+                            first, second = 'period', first
+                elif w == ',':
+                    if first == 'num':
+                        if w == word[len(word) - 1]:
+                            tokens.append(token)
+                            token = ","
+                        else:
+                            token += w
+                            first, second = 'comma', first
+                    else:
+                        tokens.append(token)
+                        tokens.append(',')
+                        token = ""
+                elif w == "'":
+                    tokens.append(token)
+                    token = "'"
+                    first, second = 'apos', ""
+                elif w.isupper():
+                    token += w.lower()
+                    first, second = 'cap', first
+                elif w == '/':
+                    if first == 'num':
+                        token += w
+                        first, second = 'slash', first
+                    elif first == "":
+                        tokens.append('/')
+                        first, second = "", ""
+                    elif first == "letter":
+                        tokens.append(token)
+                        token = ""
+                        tokens.append('/')
+                        first, second = 'slash', ""
+                elif w == '-':
+                    if first == 'letter' or first == 'num':
+                        token += w
+                        first, second = 'dash', first
+                    elif first == 'period':
+                        tokens.append(token)
+                        token = w
+                        first, second = 'dash', ""
+                    elif first == 'dash':
+                        token += w
+                        first, second = 'dash', first
+                elif w == '(':
+                    if first == 'letter' or first == 'num':
+                        token += w
+                        first, second = 'para1', first
+                    else:
+                        tokens.append('(')
+                elif w == ')':
+                    if second == 'para1':
+                        token += w
+                        first, second = 'para2', first
+                    else:
+                        tokens.append(token)
+                        token = ""
+                        tokens.append(')')
+                elif w.isdigit():
+                    token += w
+                    first, second = 'num', first
+                else:
+                    if second == 'dash' and first == 'dash':
+                        tokens.append(token)
+                        token = w
+                        first, second = 'letter', ""
+                    else:
+                        token += w
+                        first, second = 'letter', first
+
+            if not token == "":
+                tokens.append(token)
+            first = ""
+            second = ""
+            token = ""
+    
+    # print(tokens)
+    return tokens
+
+
+    # return list
+
+def decontract(line):
+    contractions = {
+        "ain't": "am not / are not",
+        "aren't": "are not / am not",
+        "can't": "cannot",
+        "can't've": "cannot have",
+        "'cause": "because",
+        "could've": "could have",
+        "couldn't": "could not",
+        "couldn't've": "could not have",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hadn't've": "had not have",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he'd": "he would",
+        "he'd've": "he would have",
+        "he'll": "he will",
+        "he'll've": "he will have",
+        "he's": "he is",
+        "how'd": "how did",
+        "how'd'y": "how do you",
+        "how'll": "how will",
+        "how's": "how is",
+        "i'd": "I would",
+        "i'd've": "I would have",
+        "i'll": "I will",
+        "i'll've": "I will have",
+        "i'm": "I am",
+        "i've": "I have",
+        "isn't": "is not",
+        "it'd": "it would",
+        "it'd've": "it would have",
+        "it'll": "it will",
+        "it'll've": "iit will have",
+        "it's": "it is",
+        "let's": "let us",
+        "ma'am": "madam",
+        "mayn't": "may not",
+        "might've": "might have",
+        "mightn't": "might not",
+        "mightn't've": "might not have",
+        "must've": "must have",
+        "mustn't": "must not",
+        "mustn't've": "must not have",
+        "needn't": "need not",
+        "needn't've": "need not have",
+        "o'clock": "of the clock",
+        "oughtn't": "ought not",
+        "oughtn't've": "ought not have",
+        "shan't": "shall not",
+        "sha'n't": "shall not",
+        "shan't've": "shall not have",
+        "she'd": "she would",
+        "she'd've": "she would have",
+        "she'll": "she will",
+        "she'll've": "she will have",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "shouldn't've": "should not have",
+        "so've": "so have",
+        "so's": "so is",
+        "that'd": "that would",
+        "that'd've": "that would have",
+        "that's": "that is",
+        "there'd": "there would",
+        "there'd've": "there would have",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'd've": "they would have",
+        "they'll": "they will",
+        "they'll've": "they will have",
+        "they're": "they are",
+        "they've": "they have",
+        "to've": "to have",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we'd've": "we would have",
+        "we'll": "we will",
+        "we'll've": "we will have",
+        "we're": "we are",
+        "we've": "we have",
+        "weren't": "were not",
+        "what'll": "what will",
+        "what'll've": "what will have",
+        "what're": "what are",
+        "what's": "what is",
+        "what've": "what have",
+        "when's": "when is",
+        "when've": "when have",
+        "where'd": "where did",
+        "where's": "where is",
+        "where've": "where have",
+        "who'll": "who will",
+        "who'll've": "who will have",
+        "who's": "who is",
+        "who've": "who have",
+        "why's": "why is",
+        "why've": "why have",
+        "will've": "will have",
+        "won't": "will not",
+        "won't've": "will not have",
+        "would've": "would have",
+        "wouldn't": "would not",
+        "wouldn't've": "would not have",
+        "y'all": "you all",
+        "y'all'd": "you all would",
+        "y'all'd've": "you all would have",
+        "y'all're": "you all are",
+        "y'all've": "you all have",
+        "you'd": "you would",
+        "you'd've": "you would have",
+        "you'll": "you will",
+        "you'll've": "you will have",
+        "you're": "you are",
+        "you've": "you have"
+    }
+    
+    for word in line.split():
+        if word.lower() in contractions:
+                line = line.replace(word, contractions[word.lower()])
+
+    return line
+
+
+
+
+
+def removeStopwords(tokens):
+    """Function that removes the stopwords. """
+
+    tokens = [word for word in tokens if word not in stopwords]
+    
+    return tokens
+    
+
+def stemWords(tokens):
+    """Function that stems the words. """
+    # use porter stemmer 
+    #  https://tartarus.org/martin/PorterStemmer/python.txt
+
+    p = PorterStemmer()
+    for index, word in enumerate(tokens):
+        tokens[index] = p.stem(word, 0, len(word) - 1)
+
+    return tokens
+
+
+
+stopwords = []
+
+def main(directory):
+    """Main functon."""
+
+    s = open('stopwords', 'r')
+    Words = s.readlines()
+    for w in Words:
+        stopwords.append(w.strip('\n'))
+
+    global_tokens = {}
+
+    # go through every file in directory provided
+    for filename in os.listdir(directory):
+        with open(os.path.join(directory, filename), 'r') as f: 
+            Lines = f.readlines()
+            for line in Lines:
+                line = removeSGML(line)
+                tokens = tokenizeText(line)
+                if tokens:
+                    tokens = removeStopwords(tokens)
+                    tokens = stemWords(tokens)
+                    for token in tokens:
+                        if token in global_tokens:
+                            global_tokens[token] += 1
+                        else:
+                            global_tokens[token] = 1
+    
+
+    # Get stats on collection
+    total_num = 0
+    total_unique = 0
+
+    for token in global_tokens:
+        if any(c.isalpha() for c in token) or any(c.isdigit() for c in token):
+            # if token.isalpha() or token.isdigit():
+            total_num += global_tokens[token]
+            total_unique += 1
+
+    global_tokens = sorted(global_tokens.items(), key=lambda kv: kv[1], reverse=True)
+    top_50 = []
+    for token in global_tokens:
+        if token[0].isalpha() or token[0].isdigit():
+            top_50.append(token)
+        if len(top_50) == 50:
+            break
+    
+    out_file = open("preprocess.output", "w")
+    print("Words " + str(total_num), file=out_file)
+    print("Vocabulary " + str(total_unique), file=out_file)
+    print("Top 50 words", file=out_file)
+
+
+    for token in top_50:
+        print(str(token[0]) + " " + str(token[1]), file=out_file)
+
+
+
+
+
+
+
+# def subset(directory):
+#     global_tokens = {}
+
+#     with open(os.path.join(directory, "cranfield0011"), 'r') as f: 
+#             Lines = f.readlines()
+#             for line in Lines:
+#                 line = removeSGML(line)
+#                 tokens = tokenizeText(line)
+#                 if tokens:
+#                     tokens = removeStopwords(tokens)
+#                     tokens = stemWords(tokens)
+#                     for token in tokens:
+#                         if token in global_tokens:
+#                             global_tokens[token] += 1
+#                         else:
+#                             global_tokens[token] = 1
+
+#     # Get stats on collection
+#     total_num = 0
+#     total_unique = 0
+
+#     for token in global_tokens:
+#         if any(c.isalpha() for c in token) or any(c.isdigit() for c in token):
+#             # if token.isalpha() or token.isdigit():
+#             total_num += global_tokens[token]
+#             total_unique += 1
+
+#     global_tokens = sorted(global_tokens.items(), key=lambda kv: kv[1], reverse=True)
+
+
+#     print("Words " + str(total_num))
+#     print("Vocabulary " + str(total_unique))
+
+#     global_tokens = {}
+#     with open(os.path.join(directory, "cranfield0037"), 'r') as f: 
+#         Lines = f.readlines()
+#         for line in Lines:
+#             line = removeSGML(line)
+#             tokens = tokenizeText(line)
+#             if tokens:
+#                 tokens = removeStopwords(tokens)
+#                 tokens = stemWords(tokens)
+#                 for token in tokens:
+#                     if token in global_tokens:
+#                         global_tokens[token] += 1
+#                     else:
+#                         global_tokens[token] = 1
+    
+
+#     # Get stats on collection
+#     total_num = 0
+#     total_unique = 0
+
+#     for token in global_tokens:
+#         if any(c.isalpha() for c in token) or any(c.isdigit() for c in token):
+#             # if token.isalpha() or token.isdigit():
+#             total_num += global_tokens[token]
+#             total_unique += 1
+
+#     global_tokens = sorted(global_tokens.items(), key=lambda kv: kv[1], reverse=True)
+
+
+#     print("Words " + str(total_num))
+#     print("Vocabulary " + str(total_unique))
+
+
+
+
+if __name__ == '__main__':
+    # run main
+    # python3 preprocess.py cranfieldDocs
+    main(sys.argv[1])
+    # subset(sys.argv[1])
